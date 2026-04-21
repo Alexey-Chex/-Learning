87aefec7887fe56a669f11568649eb9be850acd8
AlexeyChex <leshachex@mail.ru>
2026-04-21 13:32:12 +0300

    if.else zadanie_1

---
+++ if.else/zadanie_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int first, second;
+    cout << "first = ";
+    cin >> first;
+
+    cout << "second = ";
+    cin >> second;
+
+    if(first == second){
+        cout << "Chisla ravni";
+    }
+    else {
+        cout << "chisla ne ravni";
+    }
+
+
+}
